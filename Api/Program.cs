c23e633df030635d2064f23399081c9ac6bb7a46
linewelder <redalerm@gmail.com>
2024-01-28 11:17:34 +0100

    Remove call to AddEndpointsApiExplorer

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
