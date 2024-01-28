2862516f934a4af0f5addaaed7ad45601581afd3
linewelder <redalerm@gmail.com>
2024-01-28 11:43:38 +0100

    Move WeatherForecast to Models

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Models;
 
 namespace Reservant.Api.Controllers;
 
@@ -11,11 +12,6 @@ public class WeatherForecastController : Controller
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     ];
 
-    private record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
-
     /// <summary>
     /// Generate five random weather forecasts.
     /// </summary>
@@ -26,11 +22,11 @@ public class WeatherForecastController : Controller
         return Ok(Enumerable.Range(1, 5)
             .Select(index =>
                 new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    Summaries[Random.Shared.Next(Summaries.Length)]
-                ))
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
             .ToArray());
     }
 }
---
+++ Api/Models/WeatherForecast.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Reservant.Api.Models;
+
+public class WeatherForecast
+{
+    [Key]
+    public DateOnly Date { get; init; }
+
+    [Range(-100, 100)]
+    public int TemperatureC { get; init; }
+
+    [StringLength(20)]
+    public string? Summary { get; init; }
+}
