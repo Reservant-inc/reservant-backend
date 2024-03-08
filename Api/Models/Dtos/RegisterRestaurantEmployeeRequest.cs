c7b4d0949dab285922aaf381b19df532f029ae5c
Michal-Gi <s24333@pjwstk.edu.pl>
2024-03-08 23:57:30 +0100

    Added Employee DTO
    Added Employee DTO for registration controller

---
+++ Api/Models/Dtos/RegisterRestaurantEmployeeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reservant.Api.Models.Dtos
+{
+    public class RegisterRestaurantEmployeeRequest
+    {
+        [Required, StringLength(30)]
+        public required string FirstName { get; init; }
+        [Required, StringLength(30)]
+        public required string LastName { get; init; }
+        [Required, StringLength(50)]
+        public required string Email { get; init; }
+        [Required, StringLength(15), Phone]
+        public required string PhoneNumber { get; init; }
+    }
+}
