09048f06465d3bc191089a9f3ac8443faa564759
linewelder <redalerm@gmail.com>
2024-03-08 13:26:06 +0100

    Add properties from CLD to User

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -1,8 +1,67 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Reservant.Api.Models;
 
+/// <summary>
+/// One class for all user classes.
+/// </summary>
 public class User : IdentityUser
 {
+    /// <summary>
+    /// Imię.
+    /// </summary>
+    [Required, ProtectedPersonalData, StringLength(30)]
+    public required string FirstName { get; set; }
+
+    /// <summary>
+    /// Nazwisko.
+    /// </summary>
+    [Required, ProtectedPersonalData, StringLength(30)]
+    public required string LastName { get; set; }
+
+    /// <summary>
+    /// Data rejestracji.
+    /// </summary>
+    [Required]
+    public required DateTime RegisteredAt { get; set; }
+
+    /// <summary>
+    /// Data urodzenia. For Customers.
+    /// </summary>
+    public DateOnly? BirthDate { get; set; }
+
+    /// <summary>
+    /// Reputacja. For Customers.
+    /// </summary>
+    public int? Reputation { get; set; }
+
+    /// <summary>
+    /// Wiek. For Customers.
+    /// </summary>
+    public int? Age
+    {
+        get
+        {
+            if (BirthDate is null)
+            {
+                return null;
+            }
+
+            var today = DateTime.Today;
+
+            var age = today.Year - BirthDate.Value.Year;
+            if (today.DayOfYear < BirthDate.Value.DayOfYear)
+            {
+                age -= 1;
+            }
+
+            return age;
+        }
+    }
+
+    /// <summary>
+    /// Stan portfela. For Customers.
+    /// </summary>
+    public decimal? WalletBalance => throw new NotImplementedException();
 }
