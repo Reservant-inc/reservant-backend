cee4479467e6cde6e17158721437a8b9cccdcb32
s24845 <s24845@pjwstk.edu.pl>
2024-03-19 23:11:20 +0100

    Added Controller, Service - WIP
    Not working yet - work in progress

--- Api/Controllers/MyRestaurantGroupsController.cs
+++ Api/Controllers/MyRestaurantGroupsController.cs
@@ -1,6 +1,45 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Identity;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos;
+using Reservant.Api.Services;
+
 namespace Reservant.Api.Controllers;
 
-public class MyRestaurantGroupsController
+/// <summary>
+/// Managing Restaurant Groups
+/// </summary>
+[ApiController, Route("/my-restaurant-groups")]
+public class MyRestaurantGroupsController(RestaurantGroupService groupService, UserManager<User> userManager) : Controller
 {
+
+    /// <summary>
+    /// Updates restaurant group information - RestaurantOwner only
+    /// </summary>
+    [HttpPut("{id:int}"), Authorize(Roles = Roles.RestaurantOwner)]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(400)]
+    public async Task<ActionResult> UpdateRestaurantGroupInfo(int id, UpdateRestaurantGroupRequest request)
+    {
+        var user = await userManager.GetUserAsync(User);
+        var result = await groupService.UpdateRestaurantGroupAsync(id, request, user.Id);
+
+        if (!result.IsError)
+        {
+            return Ok(result.Value);
+        }
+
+        if (result.Errors != null && result.Errors.Any())
+        {
+            return BadRequest(new { errors = result.Errors.Select(e => e.ErrorMessage) });
+        }
+        
+        return BadRequest("An unexpected error occurred.");
+    }
+
     
 }
