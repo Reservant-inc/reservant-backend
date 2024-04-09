9cdbf05048749ba84d4fb4f95a548614c618ccf8
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-09 18:10:15 +0200

    Created MyMenuItem Controller

---
+++ Api/Controllers/MyMenuItemController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reservant.Api.Controllers;
+
+/// <summary>
+/// Registration and signing in and out.
+/// </summary>
+[ApiController, Route("/my-menu-item")]
+public class MyMenuItemController
+{
+    
+    
+    
+    
+    
+    
+}
