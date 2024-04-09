b32c03851a6542026ad9bdf99a3d9b2c5c3e2865
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-10 00:11:31 +0200

    Added post

--- Api/Controllers/MyMenuItemController.cs
+++ Api/Controllers/MyMenuItemController.cs
@@ -2,6 +2,7 @@
 using Reservant.Api.Models.Dtos.Menu;
 using Reservant.Api.Models.Dtos.MenuItem;
 using Reservant.Api.Services;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Controllers;
 
@@ -11,20 +12,57 @@ namespace Reservant.Api.Controllers;
 [ApiController, Route("/my-restaurants")]
 public class MyMenuItemController(RestaurantMenuService service) : Controller
 {
-
     
     /// <summary>
     /// Get list of menus by given restaurant id
     /// </summary>
     /// <param name="id">Id of the restaurant.</param>
     /// <returns></returns>
-    [HttpGet("{id}/menus")]
+    [HttpGet("{id:int}/menus")]
     [ProducesResponseType(200)]
     [ProducesResponseType(404)]
     public async Task<ActionResult<List<MenuVM>>> GetMenusById(int id)
     {
-        var result = await service.GetMenusById(id);
+        var result = await service.GetMenusAsync(id);
         
+        return Ok(result);
+    }
+    
+    
+    /// <summary>
+    /// Gets a single menu with details for a given menu ID and restaurant ID.
+    /// </summary>
+    /// <param name="restaurantId">The ID of the restaurant.</param>
+    /// <param name="menuId">The ID of the menu.</param>
+    /// <returns></returns>
+    [HttpGet("{restaurantId:int}/menus/{menuId:int}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<List<MenuVM>>> GetSingleMenuById(int restaurantId, int menuId)
+    {
+        var result = await service.GetSingleMenuAsync(restaurantId, menuId);
+        
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Posts menu to the restaurant
+    /// </summary>
+    /// <param name="req">Request for Menu to be created.</param>
+    /// <returns></returns>
+    [HttpPost("{id:int}/menus")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult> CreateMenu(CreateMenuRequest req)
+    {
+        var result = await service.PostMenuToRestaurant(req);
+
+        // if (result.IsError)
+        // {
+        //     ValidationUtils.AddErrorsToModel(result.Errors!, ModelState);
+        //     return ValidationProblem();
+        // }
+
         return Ok(result);
     }
     
--- Api/Services/RestaurantMenuService.cs
+++ Api/Services/RestaurantMenuService.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Reservant.Api.Data;
+using Reservant.Api.Models;
 using Reservant.Api.Models.Dtos.Menu;
 using Reservant.Api.Models.Dtos.MenuItem;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Services;
 
@@ -12,7 +17,13 @@ namespace Reservant.Api.Services;
 /// <param name="context">context</param>
 public class RestaurantMenuService(ApiDbContext context)
 {
-    public async Task<List<MenuVM>> GetMenusById(int id)
+    
+    /// <summary>
+    /// Returns a list of menus of specific restaurant
+    /// </summary>
+    /// <param name="id"> Id of the restaurant.</param>
+    /// <returns></returns>
+    public async Task<List<MenuVM>> GetMenusAsync(int id)
     {
         var menus = await context.Menus
             .Where(m => m.RestaurantId == id)
@@ -35,5 +46,68 @@ public class RestaurantMenuService(ApiDbContext context)
 
         return menus;
     }
+    
+    /// <summary>
+    /// Returns a menu with given Id of specific restaurant
+    /// </summary>
+    /// <param name="menuId"> Id of the menu.</param>
+    /// <param name="restaurantId"> Id of the restaurant.</param>
+    /// <returns></returns>
+    public async Task<MenuVM> GetSingleMenuAsync(int restaurantId, int menuId)
+    {
+        var menu = await context.Menus
+            .Include(m => m.MenuItems)
+            .Where(m => m.Id == menuId && m.RestaurantId == restaurantId)
+            .Select(m => new MenuVM
+            {
+                Id = m.Id,
+                MenuType = m.MenuType,
+                DateFrom = m.DateFrom,
+                DateUntil = m.DateUntil,
+                MenuItems = m.MenuItems.Select(mi => new MenuItemSummaryVM
+                {
+                    Id = mi.Id,
+                    Name = mi.Name,
+                    Price = mi.Price,
+                    AlcoholPercentage = mi.AlcoholPercentage
+                }).ToList()
+            })
+            .FirstOrDefaultAsync();
+
+        return menu;
+    }
+
+
+    /// <summary>
+    /// Posts menu to the restaurant
+    /// </summary>
+    /// <param name="req">Request for Menu to be created.</param>
+    /// <returns></returns>
+    public async Task<Result<Menu>> PostMenuToRestaurant(CreateMenuRequest req)
+    {
+        var restaurant = await context.Restaurants.FindAsync(req.RestaurantId);
+        // var errors = new List<ValidationResult>();
+        //     
+        // if (restaurant == null)
+        // {
+        //     errors.Add(new ValidationResult($"Restaurant with id: {req.RestaurantId} not found.", [nameof(req.RestaurantId)]));
+        //     return errors;
+        // }
+
+        var newMenu = new Menu
+        {
+            MenuType = req.MenuType,
+            DateFrom = req.DateFrom,
+            DateUntil = req.DateUntil,
+            RestaurantId = req.RestaurantId
+        };
+
+        context.Menus.Add(newMenu);
+        await context.SaveChangesAsync();
+
+        return newMenu;
+    }
+
+    
     
 }
