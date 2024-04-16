e7ba3eddeef6fe1021d13ac8fde1f07caeeacd28
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-16 16:25:15 +0200

    Created request, methods

---
+++ Api/Models/Dtos/RestaurantGroup/UpdateRestaurantGroupRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reservant.Api.Models.Dtos.RestaurantGroup;
+
+public class UpdateRestaurantGroupRequest
+{
+    [Required, StringLength(50)]
+    public required string Name { get; init; }
+}
