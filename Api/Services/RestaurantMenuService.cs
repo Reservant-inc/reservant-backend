fc537150bc1dc998f0b282984e71f74d75c1969f
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-23 22:43:10 +0200

    Fixed menuItem ownership check

--- Api/Services/RestaurantMenuService.cs
+++ Api/Services/RestaurantMenuService.cs
@@ -167,7 +167,7 @@ public class RestaurantMenuService(ApiDbContext context)
             .Where(item => request.ItemIds.Contains(item.Id))
             .ToListAsync();
 
-        var allBelongToUser = menuItemsToAdd.All(mi => mi.Restaurant.Group.OwnerId == user.Id);
+        var allBelongToUser = menuItemsToAdd.All(mi => mi.RestaurantId == menuToUpdate.RestaurantId);
         if (!allBelongToUser)
         {
             errors.Add(new ValidationResult("Some of menuItems does not belong to the user", [nameof(request.ItemIds)]));
