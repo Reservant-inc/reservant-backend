86c5ea34dcdd3d8c69a056f81dcfa02afe926b1b
Aleksander Babij <aleksander.babij@gmail.com>
2024-05-04 11:05:21 +0200

    added method

--- Api/Controllers/RestaurantTagsController.cs
+++ Api/Controllers/RestaurantTagsController.cs
@@ -1,6 +1,9 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Reservant.Api.Data;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Restaurant;
 
 namespace Reservant.Api.Controllers;
 
@@ -17,5 +20,15 @@ public class RestaurantTagsController(ApiDbContext context) : Controller
     public async Task<ActionResult<List<string>>> GetAll()
     {
         return Ok(await context.RestaurantTags.Select(rt => rt.Name).ToListAsync());
+    }
+
+    /// <summary>
+    /// Gets all restaurants with the given tag
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("{tag:string}/restaurants")]
+    public async Task<ActionResult<List<RestaurantSummaryVM>>> GetRestaurantsWithTag(string tag)
+    {
     }
 }
