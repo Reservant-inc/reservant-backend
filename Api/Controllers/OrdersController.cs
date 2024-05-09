d25d0b20b3eece5f70166fae473e30600aec5469
linewelder <redalerm@gmail.com>
2024-05-09 23:16:56 +0200

    Add OrdersController

---
+++ Api/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reservant.Api.Controllers;
+
+/// <summary>
+/// Managing orders
+/// </summary>
+[ApiController, Route("/orders")]
+public class OrdersController : Controller
+{
+}
