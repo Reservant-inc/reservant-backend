d3ceae76f9f8c034b5fa7d067773c0f44d9a1f30
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-05-12 09:07:48 +0200

    Updated CreateVisitAsync

--- Api/Services/VisitService.cs
+++ Api/Services/VisitService.cs
@@ -1,17 +1,54 @@
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
+using Reservant.Api.Data;
+using Reservant.Api.Models;
 using Reservant.Api.Models.Dtos.Visit;
 using Reservant.Api.Validation;
+using Reservant.Api.Validators;
 
 namespace Reservant.Api.Services;
 
 /// <summary>
 /// Service for managing visits
 /// </summary>
-public class VisitService
+public class VisitService(
+    ApiDbContext context,
+    ValidationService validationService
+    )
 {
 
-    public Task<Result<VisitVM>> CreateVisitAsync(CreateVisitRequest request)
+    public async Task<Result<VisitSummaryVM>> CreateVisitAsync(CreateVisitRequest request, User user)
     {
-        return null;
+        var restaurant = await context.Restaurants.FindAsync(request.RestaurantId);
+
+        if (restaurant is null)
+        {
+            return new ValidationFailure()
+            {
+                PropertyName = nameof(request.RestaurantId),
+                ErrorCode = $"Restaurant with ID {request.RestaurantId} not found.",
+                ErrorMessage = ErrorCodes.NotFound
+            };
+        }
+        
+        var result = await validationService.ValidateAsync(request);
+        if (!result.IsValid)
+        {
+            return result;
+        }
+
+        var visit = new Visit()
+        {
+
+        };
+        
+        return new VisitSummaryVM()
+        {
+            ClientId = user.Id,
+            Date = request.Date,
+            Takeaway = request.Takeaway,
+            RestaurantId = request.RestaurantId
+        };
     }
     
     
