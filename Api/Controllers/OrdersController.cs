928bae872e95914dc946d425f2a78e080fe4182f
DummSmartass <113634707+DummSmartass@users.noreply.github.com>
2024-05-17 21:08:50 +0200

    Feature-MichalT_POST/orders/{id}/cancel

--- Api/Controllers/OrdersController.cs
+++ Api/Controllers/OrdersController.cs
@@ -1,5 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Reservant.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Identity;
+using Reservant.Api.Models;
+using Reservant.Api.Validation;
+using Reservant.Api.Services;
+
 
 namespace Reservant.Api.Controllers;
 
@@ -7,6 +15,26 @@ namespace Reservant.Api.Controllers;
 /// Managing orders
 /// </summary>
 [ApiController, Route("/orders")]
-public class OrdersController(OrderService orderService) : Controller
+public class OrdersController(OrderService orderService, UserManager<User> userManager) : Controller
 {
+    /// <summary>
+    /// Controller responsible for canceling orders
+    /// </summary>
+    /// <request code="404"> Order doesnt exist among unrealised orders </request>
+    /// <request code="200"> Succesful cancelation </request>
+    [HttpPost("{id:int}/cancel")]
+    [ProducesResponseType(200), ProducesResponseType(404)]
+    public async Task<ActionResult> CancelOrder(int id)
+    {
+        var user = await userManager.GetUserAsync(User);
+        var result = await orderService.CancelOrderAsync(id, user);
+        if (result.IsError)
+        {
+            return result.ToValidationProblem();
+        }
+
+        if (result.Value)
+            return Ok();
+        return NotFound();     
+    }
 }
--- Api/Services/OrderService.cs
+++ Api/Services/OrderService.cs
@@ -1,8 +1,45 @@
+using Microsoft.EntityFrameworkCore;
 namespace Reservant.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Reservant.Api.Data;
+using Reservant.Api.Models;
+using Reservant.Api.Validation;
+
 
 /// <summary>
 /// Service for managing orders
 /// </summary>
-public class OrderService
+public class OrderService(ApiDbContext context)
 {
+    /// <summary>
+    /// Returns a list of restaurants owned by the user.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public async Task<Result<bool>> CancelOrderAsync(int id, User user)
+    {
+        var userId = user.Id;
+
+        var result = await context
+            .Orders
+            .Include(o => o.Visit)
+            .Include(o => o.OrderItems)
+            .Where(o => o.Id == id && o.Visit!=null?o.Visit.ClientId == userId:false)
+            .Where(o => o.OrderItems!=null?(!o.OrderItems.Any(oi => oi.Status == OrderStatus.Taken) && !o.OrderItems.Any(oi => oi.Status == OrderStatus.Cancelled)):false)
+            .FirstOrDefaultAsync();
+
+        if (result == null)
+            return false;
+
+        foreach (var orderItem in result.OrderItems)
+        {
+            orderItem.Status = OrderStatus.Cancelled;
+        }
+
+        await context.SaveChangesAsync();
+
+        return true;
+    }
+
 }
