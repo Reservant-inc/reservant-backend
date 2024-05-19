0861b7717d2e07053e6a7d9179482de6d1da172a
Aleksander Babij <aleksander.babij@gmail.com>
2024-05-19 15:39:06 +0200

    GET /orders/{id}

--- Api/Controllers/OrdersController.cs
+++ Api/Controllers/OrdersController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Models.Dtos.Order;
 using Reservant.Api.Services;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Controllers;
 
@@ -9,4 +12,19 @@ namespace Reservant.Api.Controllers;
 [ApiController, Route("/orders")]
 public class OrdersController(OrderService orderService) : Controller
 {
+
+    [HttpGet("{id:int}")]
+    [Authorize]
+    public async Task<ActionResult<OrderVM>> GetOrderById(int id)
+    {
+        var order = await orderService.GetOrderById(id);
+
+        if (order.IsError)
+        {
+            return order.ToValidationProblem();
+        }
+
+        return Ok(order.Value);
+    }
+
 }
--- Api/Services/OrderService.cs
+++ Api/Services/OrderService.cs
@@ -1,8 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Reservant.Api.Data;
+using Reservant.Api.Models.Dtos.Order;
+using Reservant.Api.Models.Dtos.OrderItem;
+using Reservant.Api.Validation;
+using System.ComponentModel.DataAnnotations;
+
 namespace Reservant.Api.Services;
 
 /// <summary>
 /// Service for managing orders
 /// </summary>
-public class OrderService
+public class OrderService(ApiDbContext context)
 {
+
+    public async Task<Result<OrderVM>> GetOrderById(int id)
+    {
+
+        var errors = new List<ValidationResult>();
+
+        var order = await context.Orders
+            .Include(o => o.OrderItems!)
+            .ThenInclude(oi => oi.MenuItem)
+            .FirstOrDefaultAsync(o => o.Id == id);
+
+        if (order == null)
+        {
+            errors.Add(new ValidationResult($"Order: {id} not found"));
+            return errors;
+        }
+
+        var orderItems = order.OrderItems?.Select(i => new OrderItemVM()
+        {
+            MenuItemId = i.MenuItemId,
+            Amount = i.Amount,
+            Cost = i.Amount * i.MenuItem?.Price ?? 0,
+            Status = i.Status,
+        }).ToList();
+
+        return new OrderVM()
+        {
+            OrderId = order.Id,
+            VisitId = order.VisitId,
+            Cost = order.Cost,
+            Status = order.Status,
+            Items = orderItems ?? [],
+            EmployeeId = order.EmployeeId
+        };
+    }
+
 }
