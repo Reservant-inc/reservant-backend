74d881285ce83eaeb0c07b0d28523b1f5f63fbed
linewelder <redalerm@gmail.com>
2024-05-30 21:02:46 +0200

    Add DTOs for Event

---
+++ Api/Models/Dtos/Event/EventSummaryVM.cs
@@ -0,0 +1,55 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Reservant.Api.Models.Dtos.Event;
+
+/// <summary>
+/// Meetup or something
+/// </summary>
+public class EventSummaryVM
+{
+    /// <summary>
+    /// Unique ID
+    /// </summary>
+    [Key]
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Optional description
+    /// </summary>
+    public required string? Description { get; set; }
+
+    /// <summary>
+    /// When the event is going to happen
+    /// </summary>
+    public required DateTime Time { get; set; }
+
+    /// <summary>
+    /// People must join until this time
+    /// </summary>
+    public required DateTime MustJoinUntil { get; set; }
+
+    /// <summary>
+    /// ID of the user who created the event
+    /// </summary>
+    public required string CreatorId { get; set; }
+
+    /// <summary>
+    /// Navigational property for the creator
+    /// </summary>
+    public required string CreatorFullName { get; set; }
+
+    /// <summary>
+    /// ID of the restaurant where the event takes place
+    /// </summary>
+    public required int RestaurantId { get; set; }
+
+    /// <summary>
+    /// Restaurant name
+    /// </summary>
+    public required string RestaurantName { get; set; }
+
+    /// <summary>
+    /// Number of users interested
+    /// </summary>
+    public required int NumberInterested { get; set; }
+}
