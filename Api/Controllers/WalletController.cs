d9f7195d89b8e8b8c66ef1050e7fb6139271dd90
Aleksander Babij <aleksander.babij@gmail.com>
2024-06-10 22:02:00 +0200

    added create transaction

--- Api/Controllers/WalletController.cs
+++ Api/Controllers/WalletController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Wallet;
 using Reservant.Api.Services;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Controllers;
 
@@ -9,5 +12,19 @@ public class WalletController(
     WalletService walletService
 ): StrictController
 {
+    [HttpPost]
+    public async Task<ActionResult> CreateTransaction(AddMoneyRequest moneyRequest)
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        var result = await walletService.CreateTransaction(moneyRequest, user!);
+
+        if (result.IsError)
+        {
+            return result.ToValidationProblem();
+        }
+
+        return Ok();
+    }
 
 }
--- Api/Services/WalletService.cs
+++ Api/Services/WalletService.cs
@@ -1,7 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Reservant.Api.Data;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Wallet;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Services;
 
-public class WalletService(DbContext context)
+public class WalletService(
+    ApiDbContext context,
+    ValidationService validationService)
 {
+
+    public async Task<Result<PaymentTransaction>> CreateTransaction(AddMoneyRequest moneyRequest, User user)
+    {
+        var newTransaction = new PaymentTransaction
+        {
+            Title = "?",
+            Amount = moneyRequest.Amount,
+            Time = DateTime.UtcNow,
+            UserId = user.Id,
+        };
+
+        var result = await validationService.ValidateAsync(newTransaction, user.Id);
+        if (!result.IsValid)
+        {
+            return result;
+        }
+
+        context.PaymentTransactions.Add(newTransaction);
+        await context.SaveChangesAsync();
+
+        return newTransaction;
+    }
+
 }
