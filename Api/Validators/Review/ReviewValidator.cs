b8dfe8e055890de8b38b3e82214de429a77f0bb5
linewelder <redalerm@gmail.com>
2024-06-12 18:59:32 +0200

    Fix ReviewValidator

--- Api/Validators/Review/ReviewValidator.cs
+++ Api/Validators/Review/ReviewValidator.cs
@@ -10,29 +10,16 @@ public class ReviewValidator : AbstractValidator<Models.Review>
     /// <inheritdoc />
     public ReviewValidator()
     {
-        RuleFor(x => x.RestaurantId)
-            .NotEmpty();
-
-        RuleFor(x => x.AuthorId)
-            .Length(36);
-
         RuleFor(x => x.Stars)
             .NotEmpty()
             .GreaterThanOrEqualTo(1)
             .LessThanOrEqualTo(5);
-
-        RuleFor(x => x.CreatedAt)
-            .NotEmpty()
-            .Must(x=>x<= DateTime.UtcNow);
 
         RuleFor(x => x.Contents)
             .Length(0, 200);
 
         RuleFor(x => x.RestaurantResponse)
             .Length(0, 200);
-
-        RuleFor(x => x.IsDeleted)
-            .NotEmpty();
 
     }
 }
