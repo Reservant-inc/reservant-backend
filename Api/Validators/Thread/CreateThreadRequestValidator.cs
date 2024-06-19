1e7da31daf65c3c18de4acc72d4276e3dede10c5
Dzwiedzi <108011192+Dzwiedzi@users.noreply.github.com>
2024-06-19 11:01:27 +0200

    Threads funcionality

---
+++ Api/Validators/Thread/CreateThreadRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Reservant.Api.Data;
+using Reservant.Api.Models.Dtos.Thread;
+
+namespace Reservant.Api.Validators.Thread;
+public class CreateThreadRequestValidator : AbstractValidator<CreateThreadRequest>
+{
+    public CreateThreadRequestValidator(ApiDbContext context)
+    {
+        RuleFor(t => t.Title)
+            .NotEmpty()
+            .WithMessage("Title cannot be empty.");
+
+        RuleFor(t => t.ParticipantIds)
+            .NotEmpty()
+            .WithMessage("ParticipantIds cannot be empty.");
+
+        RuleForEach(t => t.ParticipantIds)
+            .MustAsync(async (id, cancellation) => await context.Users.AnyAsync(u => u.Id == id))
+            .WithMessage("One or more participant IDs are invalid.");
+    }
+}
---
+++ Api/Validators/Thread/UpdateThreadRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Reservant.Api.Models.Dtos.Thread;
+
+namespace Reservant.Api.Validators.Thread;
+public class UpdateThreadRequestValidator : AbstractValidator<UpdateThreadRequest>
+{
+    public UpdateThreadRequestValidator()
+    {
+        RuleFor(t => t.Title)
+            .NotEmpty()
+            .WithMessage("Title cannot be empty.");
+    }
+}
