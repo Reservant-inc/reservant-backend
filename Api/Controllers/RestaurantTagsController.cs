258f5976e64e744c2c2cacfd6a881263ea5b883f
linewelder <redalerm@gmail.com>
2024-07-17 19:14:33 +0300

    Remove GET /restaurant-tags/{tag}/restaurants

--- Api/Controllers/RestaurantTagsController.cs
+++ Api/Controllers/RestaurantTagsController.cs
@@ -11,7 +11,7 @@ namespace Reservant.Api.Controllers;
 /// Restaurant tag management
 /// </summary>
 [ApiController, Route("/restaurant-tags")]
-public class RestaurantTagsController(ApiDbContext context, FileUploadService uploadService) : StrictController
+public class RestaurantTagsController(ApiDbContext context) : StrictController
 {
     /// <summary>
     /// Get all available tags
@@ -20,50 +20,5 @@ public class RestaurantTagsController(ApiDbContext context, FileUploadService up
     public async Task<ActionResult<List<string>>> GetAll()
     {
         return Ok(await context.RestaurantTags.Select(rt => rt.Name).ToListAsync());
-    }
-
-    /// <summary>
-    /// Gets all validated restaurants with the given tag
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    [Route("{tag}/restaurants")]
-    [ProducesResponseType(200), ProducesResponseType(404)]
-    public async Task<ActionResult<List<RestaurantSummaryVM>>> GetRestaurantsWithTag(string tag)
-    {
-
-        var resultTag = await context.RestaurantTags.FirstOrDefaultAsync(t => t.Name == tag);
-
-        if (resultTag == null)
-        {
-            return NotFound();
-        }
-
-        var result = await context.Restaurants
-            .Where(r => r.Tags.Contains(resultTag) && r.VerifierId != null)
-            .Include(r => r.Tags)
-            .ToListAsync();
-
-        return Ok(result.Select(r => new RestaurantSummaryVM()
-        {
-            RestaurantId = r.Id,
-            Name = r.Name,
-            Nip = r.Nip,
-            RestaurantType = r.RestaurantType,
-            Address = r.Address,
-            City = r.City,
-            Location = new Geolocation()
-            {
-                Longitude = r.Location.X,
-                Latitude = r.Location.Y
-            },
-            GroupId = r.GroupId,
-            Logo = uploadService.GetPathForFileName(r.LogoFileName),
-            Description = r.Description,
-            ReservationDeposit = r.ReservationDeposit,
-            ProvideDelivery = r.ProvideDelivery,
-            Tags = r.Tags.Select(t => t.Name).ToList(),
-            IsVerified = r.VerifierId != null
-        }).ToList());
     }
 }
