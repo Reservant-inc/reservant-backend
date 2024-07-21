76122d18e7ad32992f68e978e2751f1150a9798d
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-21 16:30:39 +0200

    Added AddFutureVisit function

--- Api/Services/DebugService.cs
+++ Api/Services/DebugService.cs
@@ -1,6 +1,11 @@
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Reservant.Api.Data;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Visit;
 using Reservant.Api.Options;
+using Reservant.Api.Validation;
 
 namespace Reservant.Api.Services;
 
@@ -9,8 +14,11 @@ namespace Reservant.Api.Services;
 /// </summary>
 public class DebugService(
     ApiDbContext context,
+    VisitService visitService,
+    OrderService orderService,
     DbSeeder dbSeeder,
-    IOptions<FileUploadsOptions> uploadOptions)
+    IOptions<FileUploadsOptions> uploadOptions
+    )
 {
     /// <summary>
     /// Remove all data including files on disk and seed the database
@@ -26,4 +34,37 @@ public class DebugService(
         await context.Database.EnsureCreatedAsync();
         await dbSeeder.SeedDataAsync();
     }
+
+    /// <summary>
+    /// Creates visit in a future
+    /// </summary>
+    public async Task AddFutureVisitAsync(User user)
+    {
+        var exampleVisit = visitService.CreateVisitAsync(
+            new CreateVisitRequest
+            {
+                Date = new DateTime(2030, 3, 23, 14, 0, 0),
+                NumberOfGuests = 2,
+                Participants = [user.Id],
+                RestaurantId = 1,
+                TableId = 1,
+                Takeaway = false,
+                Tip = new decimal(1.50)
+            }, 
+            user
+            );
+
+        if (exampleVisit.Result.IsError)
+        {
+            throw new ValidationResult(
+                
+                )
+        }
+
+
+
+
+    }
+    
+    
 }
