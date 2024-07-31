ddb0ccc1b108fc031ea000ff3b94e7889ec16ffd
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-31 17:52:49 +0200

    Create CreateDeliveryRequest.cs

---
+++ Api/Models/Dtos/Delivery/CreateDeliveryRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Reservant.Api.Models.Dtos.Delivery;
+
+public class CreateDeliveryRequest
+{
+    /// <summary>
+    /// Unique identifier for the delivery record.
+    /// </summary>
+    public required int Id { get; init; }
+    
+
+    /// <summary>
+    /// Positions
+    /// </summary>
+    public required List<DeliveryPositionVM> Positions { get; init; }
+}
