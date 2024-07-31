4a9d75a2e462b57fb1de6f6fada6d0b8de80bdfc
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-31 17:33:08 +0200

    Added DeliveryPosition

---
+++ Api/Models/DeliveryPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reservant.Api.Models;
+
+public class DeliveryPosition
+{
+    [Key] 
+    public int Id { get; set; }
+
+    [Required]
+    public MenuItem MenuItem { get; set; }
+
+    [Required] public int Quantity { get; set; }
+    
+}
