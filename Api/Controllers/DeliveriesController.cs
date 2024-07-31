7c25bd42827e79f03f3d0a155a9b87cf31c079bd
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-31 17:08:59 +0200

    Added Service, Controller

---
+++ Api/Controllers/DeliveriesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Delivery;
+using Reservant.Api.Services;
+using Reservant.Api.Validation;
+
+namespace Reservant.Api.Controllers;
+
+/// <summary>
+/// Delivery controller.
+/// </summary>
+[ApiController, Route("/deliveries")]
+public class DeliveriesController(
+    UserManager<User> userManager,
+    DeliveryService deliveryService
+    ) : StrictController
+{
+
+
+    [HttpPost]
+    public async Task<ActionResult<DeliveryVM>> createDelivery(DeliveryVM deliveryVM)
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        var result = await deliveryService.createDeliveryAsync(deliveryVM, user);
+
+        if (!result.IsError)
+        {
+            return Ok(result.Value);
+        }
+        return result.ToValidationProblem();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Api/Services/DeliveryService.cs
@@ -0,0 +1,44 @@
+﻿using Reservant.Api.Data;
+using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Delivery;
+using Reservant.Api.Models.Dtos.MenuItem;
+using Reservant.Api.Validation;
+
+namespace Reservant.Api.Services;
+/// <summary>
+/// Service for managing deliveries
+/// </summary>
+/// <param name="context"></param>
+public class DeliveryService(
+    ApiDbContext context, 
+    ValidationService validationService
+    )
+{
+
+    public async Task<Result<DeliveryVM>> createDeliveryAsync(DeliveryVM deliveryVM, User user)
+    {
+        // validators? restaurants?
+        var delivery = new Delivery()
+        {
+            positions = deliveryVM.positions
+        };
+
+
+        await validationService.ValidateAsync(delivery, user.Id);
+        
+        
+        await context.Deliveries.AddAsync(delivery);
+        await context.SaveChangesAsync();
+
+
+        return new DeliveryVM()
+        {
+            Id = delivery.Id,
+            positions = delivery.positions
+        };
+    }
+    
+    
+    
+    
+}
