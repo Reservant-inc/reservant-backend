8a7ef7a1b48c2745d65d20fafd5d7dffae1a221d
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-31 17:52:54 +0200

    Create DeliveryPositionVM.cs

---
+++ Api/Models/Dtos/Delivery/DeliveryPositionVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Reservant.Api.Models.Dtos.MenuItem;
+
+namespace Reservant.Api.Models.Dtos.Delivery;
+
+public class DeliveryPositionVM
+{
+    public required int Id { get; set; }
+
+    [Required]
+    public required MenuItemVM MenuItem { get; set; }
+
+    [Required] 
+    public required int Quantity { get; set; }
+}
