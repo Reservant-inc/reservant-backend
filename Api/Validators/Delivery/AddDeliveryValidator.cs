c77a8f345edce4c2386244260109be199b3a2075
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-31 17:09:04 +0200

    Added validator

---
+++ Api/Validators/Delivery/AddDeliveryValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Reservant.Api.Data;
+using Reservant.Api.Models.Dtos.Delivery;
+using Reservant.Api.Models.Dtos.Restaurant;
+
+namespace Reservant.Api.Validators.Delivery;
+
+
+/// <summary>
+/// Validator for AddEmployeeRequest
+/// </summary>
+public class AddDeliveryValidator : AbstractValidator<DeliveryVM>
+{
+    
+    
+    /// <inheritdoc />
+    public AddDeliveryValidator(ApiDbContext context) {
+        
+        RuleFor(delivery => delivery.positions)
+            .Must(position => position.Item2 > 0)
+            .WithErrorCode(ErrorCodes.DeliveryItemTooLow)
+            .WithMessage(ErrorCodes.DeliveryItemTooLow);
+
+    }
+}
