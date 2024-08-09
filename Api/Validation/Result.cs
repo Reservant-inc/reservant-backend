09a230c240ede8c42b959d62699f157df39339a9
linewelder <redalerm@gmail.com>
2024-08-09 14:30:15 +0200

    Add Result variant without value

--- Api/Validation/Result.cs
+++ Api/Validation/Result.cs
@@ -3,6 +3,88 @@ using FluentValidation.Results;
 namespace Reservant.Api.Validation;
 
 /// <summary>
+/// Used to indicate validation errors if any.
+/// </summary>
+public readonly struct Result
+{
+    /// <summary>
+    /// Whether there are validation errors or not.
+    /// </summary>
+    public bool IsError { get; }
+
+    private readonly List<ValidationFailure>? _errors;
+
+    /// <summary>
+    /// Validation errors or null if successful.
+    /// </summary>
+    public List<ValidationFailure> Errors =>
+        IsError
+            ? _errors!
+            : throw new InvalidOperationException("Attempt to access the error list of a successful Result");
+
+    /// <summary>
+    /// Constructs a successful Result.
+    /// </summary>
+    public Result()
+    {
+        IsError = false;
+        _errors = null;
+    }
+
+    /// <summary>
+    /// Constructs a failed Result.
+    /// </summary>
+    public Result(List<ValidationFailure> errors)
+    {
+        IsError = true;
+        _errors = errors;
+    }
+
+    /// <summary>
+    /// Create a successful result
+    /// </summary>
+    /// <returns></returns>
+    public static Result Success => new();
+
+    /// <summary>
+    /// Allows to write <code>return result;</code> instead of <code>return new Result(result.Errors);</code>
+    /// </summary>
+    public static implicit operator Result(ValidationResult result) => new(result.Errors);
+
+    /// <summary>
+    /// Allows to write <code>return errors;</code> instead of <code>return new Result(errors);</code>
+    /// </summary>
+    public static implicit operator Result(List<ValidationFailure> errors) => new(errors);
+
+    /// <summary>
+    /// Create a Result with a single error
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// return new ValidationFailure
+    /// {
+    ///     PropertyName = nameof(...),
+    ///     ErrorCode = ...,
+    ///     ErrorMessage = ...
+    /// }
+    /// </code>
+    /// </example>
+    public static implicit operator Result(ValidationFailure error) => new([error]);
+
+    /// <summary>
+    /// Return the value, or throw an exception if there are validation errors.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">If there are validation errors.</exception>
+    public void OrThrow()
+    {
+        if (IsError)
+        {
+            throw new InvalidOperationException("Validation failed: " + string.Join(", ", Errors));
+        }
+    }
+}
+
+/// <summary>
 /// Used to return a value in case of success OR validation errors if any.
 /// </summary>
 /// <typeparam name="TValue">The value type in case of success.</typeparam>
