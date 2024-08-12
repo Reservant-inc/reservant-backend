3a7b92c5d748770510669692c720769cf509e81b
Aleksander Babij <aleksander.babij@gmail.com>
2024-08-12 22:01:17 +0200

    added GetDeliveryById

--- Api/Controllers/DeliveriesController.cs
+++ Api/Controllers/DeliveriesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Identity;
+using Reservant.Api.Models.Dtos.Delivery;
+using Reservant.Api.Services;
 
 namespace Reservant.Api.Controllers;
 
@@ -6,6 +10,18 @@ namespace Reservant.Api.Controllers;
 /// Controller for deliveries
 /// </summary>
 [ApiController, Route("/deliveries")]
-public class DeliveriesController(): StrictController
+public class DeliveriesController(DeliveryService deliveryService): StrictController
 {
+
+    /// <summary>
+    /// Gets the delivery with the given id 
+    /// </summary>
+    /// <param name="deliveryId"></param>
+    /// <returns>OrderVM or NotFound if order wasn't found</returns>
+    [HttpGet("/deliveries/{deliveryId::int}")]
+    [Authorize(Roles = Roles.RestaurantBackdoorsEmployee)]
+    public async Task<ActionResult<DeliveryVM>> GetDeliveryById(int deliveryId)
+    {
+        return OkOrErrors(await deliveryService.GetDeliveryById(deliveryId));
+    }
 }
--- Api/Services/DeliveryService.cs
+++ Api/Services/DeliveryService.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Reservant.Api.Data;
 using Reservant.Api.Models;
+using Reservant.Api.Models.Dtos.Delivery;
+using Reservant.Api.Models.Dtos.Ingredient;
 using Reservant.Api.Validation;
+using Reservant.Api.Validators;
 
 namespace Reservant.Api.Services;
 
@@ -13,5 +18,31 @@ public class DeliveryService(
     ApiDbContext context,
     ValidationService validationService)
 {
+    public async Task<Result<DeliveryVM>> GetDeliveryById(int deliveryId)
+    {
+        var delivery = await context.Deliveries
+            .FirstOrDefaultAsync(d => d.Id == deliveryId);
 
+        if (delivery == null)
+        {
+            return new ValidationFailure
+            {
+                PropertyName = null,
+                ErrorCode = ErrorCodes.NotFound
+            };
+        }
+
+        return new DeliveryVM
+        {
+            Id = delivery.Id,
+            OrderTime = delivery.OrderTime,
+            DeliveredTime = delivery.DeliveredTime,
+            RestaurantId = delivery.RestaurantId,
+            UserId = delivery.UserId,
+            Ingredients = delivery.Ingredients.Select(i => new IngredientVM
+            {
+                
+            })
+        };
+    }
 }
