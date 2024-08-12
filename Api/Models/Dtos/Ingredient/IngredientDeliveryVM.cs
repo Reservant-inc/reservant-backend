54d81f306a448c7b59a6fdb4673a79d5d239daa3
Aleksander Babij <aleksander.babij@gmail.com>
2024-08-12 22:02:17 +0200

    added IngredientDeliveryVM

---
+++ Api/Models/Dtos/Ingredient/IngredientDeliveryVM.cs
@@ -0,0 +1,13 @@
+﻿namespace Reservant.Api.Models.Dtos.Ingredient;
+
+public class IngredientDeliveryVM
+{
+    public int DeliveryId { get; set; }
+
+    public int IngredientId { get; set; }
+
+    public double AmountOrdered { get; set; }
+    public double? AmountDelivered { get; set; }
+    public DateTime? ExpiryDate { get; set; }
+    public required string StoreName { get; set; }
+}
