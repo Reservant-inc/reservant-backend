75e13610b03ca8d2f7c9a96080716c5a309aed33
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-08-26 16:08:25 +0200

    Created EventInviteRequest

---
+++ Api/Models/Dtos/EventInvite/EventInviteRequestVM.cs
@@ -0,0 +1,26 @@
+﻿using Reservant.Api.Models.Dtos.User;
+
+namespace Reservant.Api.Models.Dtos.EventInvite;
+
+public class EventInviteRequestVM
+{
+    /// <summary>
+    /// Date and time created
+    /// </summary>
+    public required DateTime DateSent { get; init; }
+
+    /// <summary>
+    /// Date and time when the request was read by the receiver
+    /// </summary>
+    public required DateTime? DateRead { get; init; }
+
+    /// <summary>
+    /// Date and time when the request was accepted by the receiver
+    /// </summary>
+    public required DateTime? DateAccepted { get; init; }
+
+    /// <summary>
+    /// Info about the other user, be it the sender or the receiver of the request
+    /// </summary>
+    public required UserSummaryVM OtherUser { get; init; }
+}
---
+++ Api/Models/EventInviteRequest.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reservant.Api.Models;
+
+/// <summary>
+/// Event invite
+/// </summary>
+public class EventInviteRequest
+{
+    /// <summary>
+    /// Unique ID
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Date and time created
+    /// </summary>
+    public DateTime DateSent { get; set; }
+
+    /// <summary>
+    /// Date and time when the request was read by the receiver
+    /// </summary>
+    public DateTime? DateRead { get; set; }
+
+    /// <summary>
+    /// Date and time when the request was accepted by the receiver
+    /// </summary>
+    public DateTime? DateAccepted { get; set; }
+
+    /// <summary>
+    /// Date the event request was deleted
+    /// </summary>
+    public DateTime? DateDeleted { get; set; }
+
+    /// <summary>
+    /// ID of the sender
+    /// </summary>
+    [StringLength(36)]
+    public string SenderId { get; set; } = null!;
+
+    /// <summary>
+    /// ID of the target user
+    /// </summary>
+    [StringLength(36)]
+    public string ReceiverId { get; set; } = null!;
+
+    /// <summary>
+    /// Navigational property for the user that sent the invite
+    /// </summary>
+    public User Sender { get; set; } = null!;
+
+    /// <summary>
+    /// Navigational property for the target user
+    /// </summary>
+    public User Receiver { get; set; } = null!;
+}
