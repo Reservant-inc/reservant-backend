c871d23f40a34e29f21ac374022a03b753dbb909
Michal-Gi <s24333@pjwstk.edu.pl>
2024-08-26 23:19:35 +0200

    Added review controller and service

---
+++ Api/Controllers/ReviewController.cs
@@ -0,0 +1,58 @@
+﻿using ErrorCodeDocs.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reservant.Api.Dtos.Review;
+using Reservant.Api.Identity;
+using Reservant.Api.Models;
+using Reservant.Api.Services;
+using Reservant.Api.Validators;
+
+namespace Reservant.Api.Controllers
+{
+    [ApiController, Route("/reviews")]
+    [Authorize]
+    public class ReviewController(ReviewService reviewService, UserManager<User> userManager) : StrictController
+    {
+        /// <summary>
+        /// Deletes a review with a given id, great for censorship
+        /// </summary>
+        /// <param name="id">id of the review</param>
+        /// <returns>confirmation of the action's status</returns>
+        [HttpDelete("{id:int}")]
+        [MethodErrorCodes<ReviewService>(nameof(ReviewService.DeleteReviewAsync))]
+        [ProducesResponseType(200), ProducesResponseType(400)]
+        public async Task<ActionResult> DeleteReview(int id) {
+            var user = await userManager.GetUserAsync(User);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await reviewService.DeleteReviewAsync(id, user);
+            return OkOrErrors(result);
+        }
+
+        /// <summary>
+        /// Updates a review with given id
+        /// </summary>
+        /// <param name="id">id of the review to change</param>
+        /// <param name="request">new contents of the review</param>
+        /// <returns>a visual model of the updated review</returns>
+        [HttpPut("{id:int}")]
+        [Authorize]
+        [MethodErrorCodes<ReviewService>(nameof(ReviewService.UpdateReviewAsync))]
+        [ProducesResponseType(200), ProducesResponseType(400)]
+        public async Task<ActionResult<ReviewVM>> UpdateReview(int id, CreateReviewRequest request)
+        {
+            var user = await userManager.GetUserAsync(User);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await reviewService.UpdateReviewAsync(id, user, request);
+            return OkOrErrors(result);
+        }
+    }
+}
---
+++ Api/Services/ReviewService.cs
@@ -0,0 +1,77 @@
+﻿using ErrorCodeDocs.Attributes;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Reservant.Api.Data;
+using Reservant.Api.Dtos.Review;
+using Reservant.Api.Models;
+using Reservant.Api.Validation;
+using Reservant.Api.Validators;
+
+namespace Reservant.Api.Services
+{
+    public class ReviewService(
+        UserManager<User> userManager,
+        ApiDbContext context,
+        ValidationService validationService)
+    {
+        [ErrorCode(null, ErrorCodes.NotFound)]
+        public async Task<Result> DeleteReviewAsync(int id, User user)
+        {
+            var review = await context.Reviews.FirstOrDefaultAsync(r => r.AuthorId == user.Id && r.Id == id);
+            if (review == null)
+            {
+                return new ValidationFailure
+                {
+                    ErrorCode = ErrorCodes.NotFound
+                };
+            }
+
+            var restaurant = await context.Restaurants.Include(r => r.Reviews).FirstOrDefaultAsync(r => r.Id == review.RestaurantId);
+
+            if (restaurant is not null)
+            {
+                restaurant.Reviews.Remove(review);
+            }
+            context.Reviews.Remove(review);
+            await context.SaveChangesAsync();
+            return Result.Success;
+        }
+
+        [ErrorCode(null, ErrorCodes.NotFound)]
+        [ValidatorErrorCodes<CreateReviewRequest>]
+        public async Task<Result<ReviewVM>> UpdateReviewAsync(int id, User user, CreateReviewRequest request)
+        {
+            var res = await validationService.ValidateAsync(request, user.Id);
+            if (!res.IsValid)
+            {
+                return res.Errors;
+            }
+            var review = await context.Reviews.FirstOrDefaultAsync(r => r.AuthorId == user.Id && r.Id == id);
+            if (review == null)
+            {
+                return new ValidationFailure
+                {
+                    ErrorCode = ErrorCodes.NotFound
+                };
+            }
+
+            review.Stars = request.Stars;
+            review.Contents = request.Contents;
+
+            await context.SaveChangesAsync();
+            return new ReviewVM
+            {
+                ReviewId = review.Id,
+                RestaurantId = review.Id,
+                Stars = review.Stars,
+                AuthorId = user.Id,
+                AuthorFullName = user.FullName,
+                CreatedAt = DateTime.UtcNow,
+                Contents = review.Contents,
+                AnsweredAt = review.AnsweredAt,
+                RestaurantResponse = review.RestaurantResponse
+            };
+        }
+    }
+}
