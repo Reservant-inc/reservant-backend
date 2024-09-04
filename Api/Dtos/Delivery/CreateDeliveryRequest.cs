688745aed262e0445838231701d4bb01f8a7afc5
linewelder <redalerm@gmail.com>
2024-09-04 17:39:13 +0200

    docs: Add XML docs to delivery DTOs

--- Api/Dtos/Delivery/CreateDeliveryRequest.cs
+++ Api/Dtos/Delivery/CreateDeliveryRequest.cs
@@ -2,10 +2,18 @@
 
 namespace Reservant.Api.Dtos.Delivery;
 
+/// <summary>
+/// Request to create a Delivery
+/// </summary>
 public class CreateDeliveryRequest
 {
-
+    /// <summary>
+    /// ID of the restaurant
+    /// </summary>
     public required int RestaurantId { get; set; }
 
+    /// <summary>
+    /// Ingredients ordered
+    /// </summary>
     public required List<IngredientDeliveryVM> Ingredients { get; init; }
 }
--- Api/Dtos/Delivery/DeliveryVM.cs
+++ Api/Dtos/Delivery/DeliveryVM.cs
@@ -2,12 +2,38 @@
 
 namespace Reservant.Api.Dtos.Delivery;
 
+/// <summary>
+/// Detailed info about a delivery
+/// </summary>
 public class DeliveryVM
 {
+    /// <summary>
+    /// Unique ID
+    /// </summary>
     public required int DeliveryId { get; init; }
+
+    /// <summary>
+    /// When was ordered
+    /// </summary>
     public required DateTime OrderTime { get; set; }
+
+    /// <summary>
+    /// When was delivered
+    /// </summary>
     public required DateTime? DeliveredTime { get; set; }
+
+    /// <summary>
+    /// ID of the restaurant
+    /// </summary>
     public required int RestaurantId { get; set; }
+
+    /// <summary>
+    /// ID of the user who received the delivery
+    /// </summary>
     public required string? UserId { get; set; } = null!;
+
+    /// <summary>
+    /// Ingredients delivered
+    /// </summary>
     public required List<IngredientDeliveryVM> Ingredients { get; init; }
 }
