b19d690e3c4123eb5599239e62351a9412e849b8
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-09-11 00:11:41 +0200

    Added participationRequest

---
+++ Api/Models/Dtos/Event/ParticipationRequest.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Reservant.Api.Models;
+
+/// <summary>
+/// Participation request
+/// </summary>
+public class ParticipationRequest
+{
+    /// <summary>
+    /// Unique ID
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Event ID
+    /// </summary>
+    public int EventId { get; set; }
+    
+    /// <summary>
+    /// Event navigation property
+    /// </summary>
+    public Event Event { get; set; }
+    
+    /// <summary>
+    /// User ID
+    /// </summary>
+    public string UserId { get; set; }
+    
+    /// <summary>
+    /// User navigation property
+    /// </summary>
+    public User User { get; set; }
+    
+    /// <summary>
+    /// Request date
+    /// </summary>
+    public DateTime RequestDate { get; set; }
+    
+    /// <summary>
+    /// Boolean for accept
+    /// </summary>
+    public bool IsAccepted { get; set; }
+    
+    /// <summary>
+    /// Boolean for reject
+    /// </summary>
+    public bool IsRejected { get; set; }
+}
