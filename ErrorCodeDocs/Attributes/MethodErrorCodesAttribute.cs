04d7d5937e372b89b3fa672ffe56ebd9455f33f4
linewelder <redalerm@gmail.com>
2024-09-21 11:04:49 +0200

    refactor: In MethodErrorCodesAttribute just read `method` parameter
    Since we have to pass the containing type anyway,
    and the nameof operator returns the name of the
    method, we have all the components to find the
    method info already without needing to parse
    the expression string.

--- ErrorCodeDocs/Attributes/MethodErrorCodesAttribute.cs
+++ ErrorCodeDocs/Attributes/MethodErrorCodesAttribute.cs
@@ -1,58 +1,31 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
-
-namespace Reservant.ErrorCodeDocs.Attributes;
+﻿namespace Reservant.ErrorCodeDocs.Attributes;
 
 /// <summary>
 /// Use <see cref="MethodErrorCodesAttribute{TContaining}"/> instead.
-/// 
+///
 /// Documents that an endpoint (or a method) can return error codes produced by the specified method
 /// </summary>
+/// <param name="containingType">Type that contains the method</param>
+/// <param name="method">Name of the method</param>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-public partial class MethodErrorCodesAttribute : Attribute
+public class MethodErrorCodesAttribute(Type containingType, string method) : Attribute
 {
     /// <summary>
     /// Name of the class containing the referenced method
     /// </summary>
-    public Type ContainingType { get; init; }
+    public Type ContainingType { get; } = containingType;
 
     /// <summary>
     /// Name of the referenced method
     /// </summary>
-    public string MethodName { get; init; }
-
-    /// <summary>
-    /// Construct a new MethodErrorCodesAttribute
-    /// </summary>
-    /// <param name="method">Method reference, must be in the form: nameof(ClassName.MethodName)</param>
-    /// <param name="methodAsString">Do not use. Used to get the string representation of <paramref name="method"/></param>
-    /// <exception cref="ArgumentException">If <paramref name="method"/> is in wrong format</exception>
-    public MethodErrorCodesAttribute(
-        Type containingType, string method,
-        [CallerArgumentExpression(nameof(method))] string? methodAsString = null)
-    {
-        ArgumentNullException.ThrowIfNull(methodAsString, nameof(methodAsString));
-
-        var match = MethodParameterRegex().Match(methodAsString);
-        if (!match.Success)
-        {
-            throw new ArgumentException("Argument can only be in the form: nameof(ClassName.MethodName)", nameof(method));
-        }
-
-        ContainingType = containingType;
-        MethodName = match.Groups[2].Value;
-    }
-
-    [GeneratedRegex(@"^nameof\((\w+).(\w+)\)$")]
-    private static partial Regex MethodParameterRegex();
+    public string MethodName { get; } = method;
 }
 
 /// <summary>
 /// Documents that an endpoint (or a method) can return error codes produced by the specified method
 /// </summary>
-/// <param name="method">Method reference, must be in the form: nameof(ClassName.MethodName)</param>
-/// <param name="methodAsString">Do not use. Used to get the string representation of <paramref name="method"/></param>
+/// <typeparam name="TContaining">Type that contains the method</typeparam>
+/// <param name="method">Name of the method</param>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-public class MethodErrorCodesAttribute<TContaining>(
-    string method, [CallerArgumentExpression(nameof(method))] string? methodAsString = null)
-    : MethodErrorCodesAttribute(typeof(TContaining), method, methodAsString);
+public class MethodErrorCodesAttribute<TContaining>(string method)
+    : MethodErrorCodesAttribute(typeof(TContaining), method);
