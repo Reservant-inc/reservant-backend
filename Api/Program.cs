ea043997729e3dd1fb5de5896baba432472db085
linewelder <redalerm@gmail.com>
2024-10-05 12:22:44 +0200

    fix: Add custom exception handler

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
+using Reservant.Api;
 using Reservant.Api.Data;
 using Reservant.Api.Documentation;
 using Reservant.Api.Identity;
@@ -44,6 +45,7 @@ builder.Services.AddControllers()
             new JsonStringEnumConverter());
     });
 
+builder.Services.AddCustomExceptionHandler();
 builder.Services.AddScoped<GeometryFactory>(_ =>
     NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326));
 builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
@@ -72,6 +74,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpLogging();
+app.UseExceptionHandler();
 
 using (var scope = app.Services.CreateScope())
 {
