4765297244cab03b102659c627e48aa432fd78c5
linewelder <redalerm@gmail.com>
2024-10-14 22:17:52 +0200

    feat: Add AutoMapper

--- Api/Mapping/ServiceCollectionExtensions.cs
+++ Api/Mapping/ServiceCollectionExtensions.cs
@@ -13,5 +13,26 @@ public static class ServiceCollectionExtensions
     public static void AddMappingServices(this IServiceCollection services)
     {
         services.AddSingleton<UrlService>();
+        AddAutoMapper(services);
+    }
+
+    /// <summary>
+    /// Configure and add AutoMapper to the service collection
+    /// </summary>
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        var profiles = typeof(ServiceCollectionExtensions).Assembly
+            .DefinedTypes
+            .Where(t => t.IsAssignableTo(typeof(Profile)));
+        foreach (var profile in profiles)
+        {
+            services.AddSingleton(typeof(Profile), profile);
+        }
+
+        var mappingServiceProvider = services.BuildServiceProvider(validateScopes: true);
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfiles(mappingServiceProvider.GetServices<Profile>());
+        });
     }
 }
