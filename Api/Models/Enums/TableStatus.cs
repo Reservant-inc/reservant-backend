1888f07c3e3879bb3153d24edecdd96d50ed2654
Aleksander Babij <aleksander.babij@gmail.com>
2024-11-18 17:24:21 +0100

    added table status

---
+++ Api/Models/Enums/TableStatus.cs
@@ -0,0 +1,22 @@
+namespace Reservant.Api.Models.Enums;
+
+/// <summary>
+/// Restaurant type
+/// </summary>
+public enum TableStats
+{
+    /// <summary>
+    /// Restauracja
+    /// </summary>
+    Available,
+
+    /// <summary>
+    /// Bar
+    /// </summary>
+    Taken,
+
+    /// <summary>
+    /// Kawiarnia
+    /// </summary>
+    VisitSoon
+}
