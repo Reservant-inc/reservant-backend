90361c879ddaa17c9c442087a7709c3e01c71b91
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-01-13 11:03:49 +0100

    Added dto

---
+++ Api/Dtos/Restaurants/FullRestaurantVM.cs
@@ -0,0 +1,150 @@
+﻿using System.Text.Json.Serialization;
+using Reservant.Api.Dtos.Location;
+using Reservant.Api.Dtos.Tables;
+using Reservant.Api.Models.Enums;
+
+namespace Reservant.Api.Dtos.Restaurants;
+
+/// <summary>
+/// Pełne informacje o restauracji przeznaczone dla BOK
+/// </summary>
+public class FullRestaurantVM
+{
+    /// <summary>
+    /// Unique ID
+    /// </summary>
+    public required int RestaurantId { get; init; }
+
+    /// <summary>
+    /// Nazwa
+    /// </summary>
+    /// <example>McJohn's</example>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Type of the establishment
+    /// </summary>
+    public required RestaurantType RestaurantType { get; set; }
+
+    /// <summary>
+    /// <a href="https://pl.wikipedia.org/wiki/Numer_identyfikacji_podatkowej">NIP</a> associated with the restaurant
+    /// </summary>
+    /// <example>1231264550</example>
+    public required string Nip { get; init; }
+
+    /// <summary>
+    /// Adres
+    /// </summary>
+    /// <example>ul. Koszykowa 86</example>
+    public required string Address { get; init; }
+
+    /// <summary>
+    /// Postal index of the restaurant
+    /// </summary>
+    /// <example>00-000</example>
+    public required string PostalIndex { get; init; }
+
+    /// <summary>
+    /// City of the restaurant
+    /// </summary>
+    /// <example>Warszawa</example>
+    public required string City { get; init; }
+
+    /// <summary>
+    /// Geolocation class having Longitude, Latitude
+    /// </summary>
+    /// <example></example>
+    public required Geolocation Location { get; init; }
+
+    /// <summary>
+    /// Max reservation duration in minutes
+    /// </summary>
+    public required int MaxReservationDurationMinutes { get; init; }
+
+    /// <summary>
+    /// Restaurant group ID
+    /// </summary>
+    public required int GroupId { get; set; }
+
+    /// <summary>
+    /// Restaurant group name
+    /// </summary>
+    /// <example>McJohn's Restaurant Group</example>
+    public required string GroupName { get; set; }
+
+    /// <summary>
+    /// URI of the rental contract (umowa najmu lokalu)
+    /// </summary>
+    public required string? RentalContract { get; set; }
+
+    /// <summary>
+    /// URI of the alcohol license (licencja na sprzedaż alkoholu)
+    /// </summary>
+    public required string? AlcoholLicense { get; set; }
+
+    /// <summary>
+    /// URI of the permission to conduct business (zgoda na prowadzenie działalności)
+    /// </summary>
+    public required string BusinessPermission { get; set; }
+
+    /// <summary>
+    /// URI of the ID card (dowód osobisty)
+    /// </summary>
+    public required string IdCard { get; set; }
+
+    /// <summary>
+    /// List of tables in the restaurant
+    /// </summary>
+    public required IEnumerable<TableVM> Tables { get; init; }
+
+    /// <summary>
+    /// Whether we provide delivery for the restaurant
+    /// </summary>
+    public required bool ProvideDelivery { get; init; }
+
+    /// <summary>
+    /// URI of the logo
+    /// </summary>
+    public required string Logo { get; init; }
+
+    /// <summary>
+    /// Photos of the restaurant
+    /// </summary>
+    public required List<string> Photos { get; init; }
+
+    /// <summary>
+    /// Optional description of the restaurant
+    /// </summary>
+    public required string? Description { get; init; }
+
+    /// <summary>
+    /// Deposit
+    /// </summary>
+    public required decimal? ReservationDeposit { get; init; }
+
+    /// <summary>
+    /// Hours when the restaurant is open
+    /// </summary>
+    public required List<AvailableHoursVM> OpeningHours { get; set; }
+
+    /// <summary>
+    /// Restaurant tags
+    /// </summary>
+    public required List<string> Tags { get; init; }
+        
+    /// <summary>
+    /// ID of restaurant verifier
+    /// </summary>
+    public required Guid? VerifierId { get; init; }
+    
+    /// <summary>
+    /// Whether the restaurant is verified or not
+    /// </summary>
+    public required bool IsVerified { get; init; }
+        
+    /// <summary>
+    /// Whether the restaurant is archived
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public required bool IsArchived { get; set; }
+}
