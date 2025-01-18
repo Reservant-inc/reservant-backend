0c199a3f92407d78e1646dc0b126d32ba14d3331
Michal-Gi <s24333@pjwstk.edu.pl>
2025-01-18 20:12:03 +0100

    Added message thread ids for get/friends

---
+++ Api/Dtos/FriendRequests/FriendRequestWithPrivateThreadsVM.cs
@@ -0,0 +1,34 @@
+﻿using Reservant.Api.Dtos.Users;
+
+namespace Reservant.Api.Dtos.FriendRequests;
+
+/// <summary>
+/// DTO for friend requests with private chat threads between them
+/// </summary>
+public class FriendRequestWithPrivateThreadsVM
+{
+    /// <summary>
+    /// Date and time created
+    /// </summary>
+    public required DateTime DateSent { get; init; }
+
+    /// <summary>
+    /// Date and time when the request was read by the receiver
+    /// </summary>
+    public required DateTime? DateRead { get; init; }
+
+    /// <summary>
+    /// Date and time when the request was accepted by the receiver
+    /// </summary>
+    public required DateTime? DateAccepted { get; init; }
+
+    /// <summary>
+    /// Info about the other user, be it the sender or the receiver of the request
+    /// </summary>
+    public required UserSummaryVM OtherUser { get; init; }
+
+    /// <summary>
+    /// Id of private message thread between users
+    /// </summary>
+    public required int? PrivateMessageThreadId { get; init; }
+}
